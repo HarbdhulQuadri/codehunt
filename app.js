2d0b1007e32ef871e71dc1ad5223f4b4db08044b
HarbdhulQuadri <adegbijilanre@gmail.com>
2021-02-11 02:43:15 +0100

    i love to ciommit

--- app.js
+++ app.js
@@ -4,12 +4,25 @@ const path = require ('path');
 const nodemailer = require("nodemailer");
 //init app
 app = express();
+// setting the view using the jade template engine
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine",'jade');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-
+app.use(express.static(path.join(__dirname,'public/stylesheets')));
 app.get('/' ,function(req,res){
-    res.send("Hello World");
-})
+    res.render('index');
+});
+app.get('/about' ,function(req,res){
+    res.render('about');
+});
+app.get('/contact' ,function(req,res){
+    res.render('contact');
+});
+app.post('/contact/send' ,function(req,res){
+console.log("testing our Sender")
+});
 
 
 app.listen(3000);
