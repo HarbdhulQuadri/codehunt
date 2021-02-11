48c2af5290d26896f40ab2da64bab007142cbcf0
HarbdhulQuadri <adegbijilanre@gmail.com>
2021-02-11 15:57:24 +0100

    i made a commit this evening

--- app.js
+++ app.js
@@ -21,7 +21,29 @@ app.get('/contact' ,function(req,res){
     res.render('contact');
 });
 app.post('/contact/send' ,function(req,res){
-console.log("testing our Sender")
+    transporter = nodemailer.createTransport({
+        service : 'gmail',
+        auth:{
+            user :"adegbijilanre@gmail.com",
+
+        }
+    });
+    const mailOption = {
+        from : "abdulquadri <iamlanreadegbiji@gmail.com>",
+        to : "iamlanreadegbiji@gmail.com",
+        subject :"Contact Us Informations",
+        text: "you have a new submission with the following details Name"+req.body.name+"Email"+req.body.email+"Message"+req.body.message,
+        html :'<p>you have a new submission with the following details Name...</p><ul><li>'+req.body.name+'</li><li>Email'+req.body.email+'</li><li>Message'+req.body.message+'</ul>'
+    };
+    transporter.sendMail(mailOption,function(error,info){
+        if (error){
+            console.log(error);
+            res.redirect('/');
+        }else{
+            console.log("Message Sent:"+info.response);
+            res.redirect('/');
+        }
+    })
 });
 
 
